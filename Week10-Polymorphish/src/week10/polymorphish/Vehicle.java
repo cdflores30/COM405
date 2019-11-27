811ba9345af480ec0c192b4156edfb055d7b512e
cdflores30 <4florc81@solent.ac.uk>
2019-11-27 11:36:13 +0000

    Vehicle abstract

---
+++ Week10-Polymorphish/src/week10/polymorphish/Vehicle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week10.polymorphish;
+
+/**
+ *
+ * @author Carlos Flores
+ */
+public abstract class Vehicle {
+    protected String registrationNumber;
+    protected double weight;
+    
+    public Vehicle (String registration, double weightIn){
+    registrationNumber= registration;
+    weight=weightIn;
+    }
+    
+    public abstract double calculateFee();
+    
+    public String getRegistration(){
+    return registrationNumber;
+    }
+    
+    public double getWeight(){
+    return weight;
+    }
+}
